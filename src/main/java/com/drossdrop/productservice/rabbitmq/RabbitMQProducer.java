29998ce6f8fa88dfc1ab646388172c3c89440c7b
Raphael van Marion <raphaeldarealmvp@gmail.com>
2023-06-07 09:25:48 +0200

    Refactor config

--- src/main/java/com/drossdrop/productservice/rabbitmq/RabbitMQProducer.java
+++ src/main/java/com/drossdrop/productservice/rabbitmq/RabbitMQProducer.java
@@ -3,35 +3,40 @@ package com.drossdrop.productservice.rabbitmq;
 import com.drossdrop.productservice.dto.ProductRabbitMQ;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+@Service
 public class RabbitMQProducer {
-    private final RabbitTemplate rabbitTemplate;
 
-    @Autowired
-    private Queue myQueue;
+    private static final Logger LOGGER = LoggerFactory.getLogger(RabbitMQProducer.class);
+
+    private final RabbitTemplate rabbitTemplate;
 
     @Autowired
     public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
-//    public void sendMessage(String queueName, String message) {
-//        rabbitTemplate.convertAndSend(queueName, message);
-//    }
+
+    public void sendMessage(String message) {
+        LOGGER.info("Sending message...");
+        rabbitTemplate.convertAndSend("product_exchange", "product_routing_key", message);
+    }
 
     public void sendProduct(ProductRabbitMQ product) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        String json = null;
-        try {
-            json = objectMapper.writeValueAsString(product);
-        } catch (JsonProcessingException e) {
-            throw new RuntimeException(e);
-        }
-        rabbitTemplate.convertAndSend(myQueue.getName(), json);
+        LOGGER.info(String.format("Sending message...-> %s", product.toString()));
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        String json = null;
+//        try {
+//            json = objectMapper.writeValueAsString(product);
+//        } catch (JsonProcessingException e) {
+//            throw new RuntimeException(e);
+//        }
+        rabbitTemplate.convertAndSend("product_json", "product_routing_json_key", product);
     }
 }
