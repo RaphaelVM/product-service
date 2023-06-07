38a0b3cb4f6e8bba4f78f231f61d8017cac9cf05
Raphael van Marion <raphaeldarealmvp@gmail.com>
2023-06-07 12:02:55 +0200

    Update JwtUtil.java

--- src/main/java/com/drossdrop/productservice/util/JwtUtil.java
+++ src/main/java/com/drossdrop/productservice/util/JwtUtil.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.ArrayList;
+import java.util.Base64;
+import java.util.List;
 
 @Component
 public class JwtUtil {
@@ -17,22 +19,31 @@ public class JwtUtil {
     public static final String SECRET = "5367566B59703373367639792F423F4528482B4D6251655468576D5A71347437";
 
 
-    private Claims extractClaims(String token) {
-        Jws<Claims> jws = Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    public List<String> getRolesFromJWT(String token) {
 
-        return jws.getBody();
-    }
+        // Decode the token if needed
+        String decodedToken = decodeToken(token);
 
-    public ArrayList<String> getRoleFromClaims(String token) {
-        Claims claims = extractClaims(token);
-        String role = claims.get("roles", String.class);
-        return new ArrayList<String>() {{
-            add(role);
-        }};
+        // Process the token, validate, or extract information
+        Claims claims = Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(decodedToken)
+                .getBody();
+
+        // Access the "roles" claim
+
+        // Continue with your business logic
+        List<String> roles = (List<String>) claims.get("roles");
+        return roles;
     }
 
     public void validateToken(final String token) {
         Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+    }
+
+    private String decodeToken(String token) {
+        return new String(Base64.getDecoder().decode(token));
     }
 
     private Key getSignKey() {
